2a6f4fc94760100e0f593dc4e7b431f7864bb347
Bryce Stabenow <brycestabenow617@gmail.com>
2024-04-23 09:00:26 -0500

    add expressive-code for markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import icon from "astro-icon";
+
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bryce-stabenow.dev',
-  integrations: [tailwind(), sitemap(), icon()],
+  integrations: [tailwind(), sitemap(), icon(), expressiveCode()],
   devToolbar: {
     enabled: false
   }
