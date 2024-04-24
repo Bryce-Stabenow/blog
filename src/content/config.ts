976e364661f9aa486fadec369fe3a9b0d07b65e3
Bryce Stabenow <brycestabenow617@gmail.com>
2024-04-24 08:55:17 -0500

    update font, styling on blog cards

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,13 +2,13 @@ import { z, defineCollection } from "astro:content";
 
 const blogsCollection = defineCollection({
     type: 'content',
-    schema: z.object({
+    schema: ({ image }) => z.object({
       title: z.string(),
       pubDate: z.date(),
       description: z.string(),
       author: z.string(),
       image: z.object({
-        url: z.string(),
+        url: image(),
         alt: z.string()
       }),
       tags: z.array(z.string())
